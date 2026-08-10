0efc78e8314eb7df6c8654d906aefe0b36518e8d
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-08-10 21:47:57 +0530

    Play & pause video

--- logic.js
+++ logic.js
@@ -3,3 +3,12 @@ const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+} 
